3bb2eac11ec1e2d63857a63e6c721b34a63370c5
Chloe Loca <chloe@chols-rad-mac.local>
2025-07-21 01:09:06 +0200

    WIP: Debugging blank screen issue

--- src/App.svelte
+++ src/App.svelte
@@ -1,47 +1,34 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import HospitalBill from "./lib/HospitalBill.svelte";
+  import billData from "./data/hospital-bill.json";
+
+  const bills = /** @type {any[]} */ (billData); //billData was being treated as unknown and not an array.. so this helped!
+
+  console.log("App is loaded!");
+  let msg = "Hello, Chloe!";
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <h1>{msg}</h1>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .bill-container {
+    margin: 2rem 0;
+    padding: 1rem;
+    border: 1px solid #ddd;
+    border-radius: 0.5rem;
+    background: #f9f9f9;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  h2 {
+    margin-bottom: 0.5rem;
+    color: #333;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
+
+  main {
+    padding: 2rem;
+    font-family: "Courier New", Courier, monospace;
+    background-color: #e2e2e2e4;
   }
 </style>
---
+++ src/lib/HospitalBill.svelte
@@ -0,0 +1,36 @@
+<script>
+  export let billItems = [];
+</script>
+
+<section>
+  <h2>Hospital Bill</h2>
+  <ul>
+    {#each billItems as items}
+      <li class={items.covered ? "covered" : "uncovered"}>
+        {items.description} ---- ${items.amount}
+      </li>
+    {/each}
+  </ul>
+</section>
+
+<style>
+  ul {
+    list-style: none;
+    padding: 0;
+  }
+
+  li {
+    font-size: 1.1rem;
+    margin: 0.25rem 0;
+  }
+
+  .covered {
+    text-decoration: line-through;
+    color: #aaa;
+  }
+
+  .uncovered {
+    font-weight: bold;
+    color: #c0392b;
+  }
+</style>
